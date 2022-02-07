bd15c4d71be6038a0f6f6b70007053a9089b4d7e
DiegoPereyra12 <diegopcsb95@gmail.com>
2022-02-07 11:43:40 -0600

    orientation and internet

--- app/src/main/java/com/bancoazteca/corresonsales/heroessamplee/data/network/HeroService.kt
+++ app/src/main/java/com/bancoazteca/corresonsales/heroessamplee/data/network/HeroService.kt
@@ -1,9 +1,11 @@
 package com.bancoazteca.corresonsales.heroessamplee.data.network
 
+import android.widget.Toast
 import com.bancoazteca.corresonsales.heroessamplee.core.RetrofitHelper
 import com.bancoazteca.corresonsales.heroessamplee.data.model.apimodel.ApiHeroModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class HeroService {
 
@@ -12,12 +14,17 @@ class HeroService {
 
     suspend fun getHeroById(id: Int): ApiHeroModel? {
         return withContext(Dispatchers.IO) {
-            val response = client.getHeroById(id)
-            if (response.isSuccessful && response.body()?.id != null) {
-                response.body()
-            } else {
-                null
+            try {
+                val response = client.getHeroById(id)
+                if (response.isSuccessful && response.body()?.id != null) {
+                    response.body()
+                } else {
+                    null
+                }
+            }catch (exception: Exception){
+                return@withContext null
             }
+
 
         }
 
--- app/src/main/java/com/bancoazteca/corresonsales/heroessamplee/ui/view/MainActivity.kt
+++ app/src/main/java/com/bancoazteca/corresonsales/heroessamplee/ui/view/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         heroViewModel.onCreate()
+
     }
 }
